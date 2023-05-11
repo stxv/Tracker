00264c671357deb9362dd56afaeefed25fc5bf38
Steven Tran <stevenhuyt@gmail.com>
2023-05-11 16:23:34 -0400

    Add more routes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask  
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/") 
-def hello_world():
-    return "hello world"  
+@app.route("/")
+@app.route("/home") 
+def home():
+    return "<h1>Home Page<h1>"
+
+@app.route("/about") 
+def about():
+    return "<h1>About Page<h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
